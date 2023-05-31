8a4974dd40c4541125f84b4d424b597a6b3090f0
enot3811 <enot3811@gmail.com>
2023-05-31 20:50:21 +0300

    Add sample_type to module import

--- utils/database_utils/__init__.py
+++ utils/database_utils/__init__.py
@@ -2,4 +2,4 @@
 A Package contains tools for working with dataset's data.
 """
 
-from utils.database_utils.dataset import Dataset  # noqa
+from utils.database_utils.dataset import Dataset, sample_type  # noqa
